da0166821d679afaa478e1b160fc97abee81c913
rmseq <rm.sequeira@campus.fct.unl.pt>
2023-12-03 21:21:40 +0000

    Add day 2 (unwashed version)

---
+++ Day3/rmseq/day3.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("usage: %s <path> <part>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	data, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	switch os.Args[2] {
+	case "1":
+		fmt.Println(part1(string(data)))
+	case "2":
+		fmt.Println(part2(string(data)))
+	default:
+		fmt.Printf("usage: %s <path> <part>\n", os.Args[0])
+		os.Exit(1)
+	}
+}
+
+func part1(input string) int {
+	var res int
+	lines := strings.Split(input, "\n")
+	symbols, numbers := parse(lines)
+	for p, size := range numbers {
+		if hasNeighbours(p.x, p.y, size, &symbols) {
+			val, _ := strconv.Atoi(lines[p.y][p.x : p.x+size])
+			res += val
+		}
+	}
+	return res
+}
+
+func part2(input string) int {
+	var res int
+	lines := strings.Split(input, "\n")
+	symbols, numbers := parse(lines)
+
+	gears := make(map[position][]int)
+	for p, size := range numbers {
+		neighbours(p.x, p.y, size, &symbols)
+		for _, n := range neighbours(p.x, p.y, size, &symbols) {
+			if lines[n.y][n.x] == '*' {
+				val, _ := strconv.Atoi(lines[p.y][p.x : p.x+size])
+				if _, has := gears[n]; !has {
+					gears[n] = []int{val}
+					continue
+				}
+				gears[n] = append(gears[n], val)
+			}
+		}
+	}
+	for _, vals := range gears {
+		if len(vals) == 2 {
+			res += vals[0] * vals[1]
+		}
+	}
+	return res
+}
+
+func neighbours(x, y, size int, world *map[position]int) []position {
+	var neighs []position
+	for _, i := range []int{-1, size} {
+		pos := position{x + i, y}
+		if _, has := (*world)[pos]; has {
+			neighs = append(neighs, pos)
+		}
+	}
+	for _, j := range []int{-1, 1} {
+		for i := size; i >= -1; i-- {
+			pos := position{x + i, y + j}
+			if _, has := (*world)[pos]; has {
+				neighs = append(neighs, pos)
+			}
+		}
+	}
+	return neighs
+}
+
+func hasNeighbours(x, y, size int, world *map[position]int) bool {
+	hasXNeighbours := func() bool {
+		for _, i := range []int{-1, size} {
+			if _, has := (*world)[position{x + i, y}]; has {
+				return true
+			}
+		}
+		return false
+	}
+	hasYNeighbours := func() bool {
+		for _, j := range []int{-1, 1} {
+			for i := size; i >= -1; i-- {
+				if _, has := (*world)[position{x + i, y + j}]; has {
+					return true
+				}
+			}
+		}
+		return false
+	}
+	return hasXNeighbours() || hasYNeighbours()
+}
+
+func parse(lines []string) (symbols map[position]int, numbers map[position]int) {
+	symbols, numbers = make(map[position]int), make(map[position]int)
+	for i, ln := range lines {
+		for j := 0; j < len(ln); j++ {
+			if ln[j] == '.' {
+				continue
+			}
+			p := position{j, i}
+			var size int
+			for j+size < len(ln) && unicode.IsDigit(rune(ln[j+size])) {
+				size++
+			}
+			if size != 0 {
+				numbers[p] = size
+				j += size - 1
+				continue
+			}
+			// Everything that is not a number or '.' is a symbol (symbols were not specified)
+			symbols[p] = 1
+		}
+	}
+	return // symbols, numbers
+}
+
+type position struct {
+	x, y int
+}
---
+++ Day3/rmseq/go.mod
@@ -0,0 +1,3 @@
+module github.com/oss5031/AdventOfCode2023/Day3/rmseq
+
+go 1.21.3
